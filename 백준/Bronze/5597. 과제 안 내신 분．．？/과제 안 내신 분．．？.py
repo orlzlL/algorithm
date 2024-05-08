2aaabd18f00d3c7d3c9bd21a8f635c0b700a4bb9
orlzlL <80697609+orlzlL@users.noreply.github.com>
2024-05-09 08:16:11 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,11 @@
+# 과제 안 내신 분...?
+# remove
+
+student = [i for i in range(1, 31)]
+
+for _ in range (28):
+    num = int(input())
+    student.remove(num)
+
+print(min(student))
+print(max(student))
