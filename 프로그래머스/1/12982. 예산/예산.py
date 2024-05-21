6f5add6bf01eae3b027214b72b77f89fee5deb41
orlzlL <80697609+orlzlL@users.noreply.github.com>
2024-05-22 08:26:22 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,13 @@
+def solution(d, budget):
+    answer = 0
+
+    d.sort()
+    
+    for i in range (len(d)):
+        if budget >= d[i]:
+            budget -= d[i]
+            answer += 1
+        else:
+            break
+    
+    return answer
